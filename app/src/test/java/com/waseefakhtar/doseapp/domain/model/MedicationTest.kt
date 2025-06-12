39c249bf02867081bde8e49c6e718fac27a6a473
RAMarenco <00080121@uca.edu.sv>
2025-06-11 19:35:41 -0600

    Delete unused imports

--- app/src/test/java/com/waseefakhtar/doseapp/domain/model/MedicationTest.kt
+++ app/src/test/java/com/waseefakhtar/doseapp/domain/model/MedicationTest.kt
@@ -1,6 +1,5 @@
 package com.waseefakhtar.doseapp.domain.model
 
-import android.os.Parcel
 import com.waseefakhtar.doseapp.util.MedicationType
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
